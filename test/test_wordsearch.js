891b2f7551ae24afe26cf03ffdc8b370db7e69f6
Ahmed <achau1019@gmail.com>
2021-07-28 21:12:45 +0000

    implemented wordsearch

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -31,6 +31,22 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+  
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'O', 'N', 'H', 'E', 'L', 'D'],
+      ['E', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['N', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['F', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['E', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['L', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['D', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
 
     assert.isTrue(result);
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+// function to transpose the matrix and switch colums with rows.
+const transpose = function(matrix) {
+  let array = [];
+
+  for (let i = 0; i < matrix[0].length; i++) {
+    array.push([])
+
+    for (let j = 0; j < matrix.length; j++) {
+      array[i][j] = matrix[j][i];
     }
+  }
+  return array;
+};
+
+const wordSearch = (letters, word) => { 
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  const verticalJoin = transpose(letters).map(ls => ls.join('')); 
+  
+  for (const l of horizontalJoin) {
+      if (l.includes(word)) return true;
+  }
+  
+  for(const l of verticalJoin) {
+    if(l.includes(word)) return true;
+  }
+
+  return false;
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
+
+
+
+   
